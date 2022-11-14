c486838286af6fd1e7859306d43dcd9d99964093
tony tolver <andrademaresantonio00@gmail.com>
2022-11-14 09:59:01 -0600

    primer version

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mis tareas</title>
+</head>
+<body>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem molestiae expedita optio obcaecati quasi eius omnis commodi praesentium magnam nemo? Numquam eos modi recusandae harum quam reprehenderit ullam et obcaecati!
+</body>
+</html>
